2f61c72cf2ebec89d1cd3ad50d59822bafd6a76e
vamsilva <70989112+vamsilva@users.noreply.github.com>
2024-07-28 16:55:22 -0300

    Add files via upload

---
+++ desafio-classificador-nivel-heroi.js
@@ -0,0 +1,31 @@
+
+//Criando as variáveis
+let nome = prompt("Digite o nome do herói: ")
+let xp = prompt("Digite a quantidade de experiência do herói: ")
+let nivel
+
+    //Classificando o nível do herói
+    if (xp > 0 && xp < 1000) {
+        nivel = "Ferro";
+    } else if (xp >= 1001 && xp <= 2000) {
+        nivel = "Bronze";
+    } else if (xp >= 2001 && xp <= 5000) {
+        nivel = "Prata";
+    } else if (xp >= 5001 && xp <= 7000) {
+        nivel = "Ouro";
+    } else if (xp >= 7001 && xp <= 8000) {
+        nivel = "Platina";
+    } else if (xp >= 8001 && xp <= 9000) {
+        nivel = "Ascendente";
+    } else if (xp >= 9001 && xp <= 10000) {
+        nivel = "Imortal";
+    } else if (xp >= 10001) {
+        nivel = "Radiante";
+    } else {
+        nivel = "Desconhecido. Insira um valor válido para quantidade de experiência";
+    }
+
+
+//Saída
+console.log(`O Herói de nome ${nome} está no nível ${nivel}`);
+
